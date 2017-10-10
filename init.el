9050600e754664985fa38c657b68b3ca593a51ea
Zhenyuan Liu <rubyist.liu@gmail.com>
2017-10-10 20:17:30 +0800

    add elixir layer

--- init.el
+++ init.el
@@ -50,6 +50,7 @@ values."
      syntax-checking
      version-control
      (osx :variables osx-dictionary-dictionary-choice "English")
+     elixir
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
